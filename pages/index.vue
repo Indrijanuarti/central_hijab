c49f27c047e4515844a12dfc48a1bb1d1c6ed639
Sitimudrikahh <smudrikah638@gmail.com>
2022-02-15 17:53:02 +0700

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <main>
+        <div class="container">
+          <Navigasi />
+            <div>
+              <div class="jumbotron">
+                <h1 class="text-center">Welcome to the Otahi Store</h1>
+              </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+  mounted(){
+    console.log(this.$supabase)
+  }
+}
+</script>
